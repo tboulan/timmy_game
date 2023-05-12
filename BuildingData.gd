1f7862da790baa9caec585dd22f52a66cbcfbb5b
tboulan <tboulan@gmail.com>
2023-05-12 09:55:04 -0500

    Replace building constants with enum

--- BuildingData.gd
+++ BuildingData.gd
@@ -1,17 +1,5 @@
 extends Node
-
-var base = Building.new(0, preload("res://Sprites/Base.png"), 0, 0, 0, 0)
-var mine = Building.new(1, preload("res://Sprites/Mine.png"), 2, 1, 4, 1)
-var greenhouse = Building.new(2, preload("res://Sprites/Greenhouse.png"), 1, 1, 0, 0)
-var solarPanel = Building.new(3, preload("res://Sprites/SolarPanel.png"), 4, 1, 0, 0)
-var tree = Building.new(5, preload("res://Sprites/Trees1.png"), 0, 0,0 ,0)
-
-
-# --- Building Types ---
-# base			= 0
-# mine			= 1
-# greenhouse	= 2
-# solarpanel	= 3
+enum Buildings { BASE, MINE, GREENHOUSE, SOLAR_PANEL}
 
 # --- Resources ---
 # none			= 0
@@ -19,6 +7,15 @@ var tree = Building.new(5, preload("res://Sprites/Trees1.png"), 0, 0,0 ,0)
 # metal			= 2
 # oxygen		= 3
 # energy		= 4
+
+
+var base = Building.new(Buildings.BASE, preload("res://Sprites/Base.png"), 0, 0, 0, 0)
+var mine = Building.new(Buildings.MINE, preload("res://Sprites/Mine.png"), 2, 1, 4, 1)
+var greenhouse = Building.new(Buildings.GREENHOUSE, preload("res://Sprites/Greenhouse.png"), 1, 1, 0, 0)
+var solarPanel = Building.new(Buildings.SOLAR_PANEL, preload("res://Sprites/SolarPanel.png"), 4, 1, 0, 0)
+# var tree = Building.new(5, preload("res://Sprites/Trees1.png"), 0, 0,0 ,0)
+
+
 
 class Building:
 	# building type
