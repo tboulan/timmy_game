8af6cab40641b72ac91281ea6f8be5efd8ac39d1
tboulan <tboulan@gmail.com>
2023-05-16 12:35:29 -0500

    Signed-off-by: tboulan <tboulan@gmail.com>

--- Map.gd
+++ Map.gd
@@ -6,6 +6,10 @@ var allTiles : Array
 # all the tiles which have buildings on them
 var tilesWithBuildings : Array
 
+# tiles with buildings to be checked if legal
+var tilesToHighlight : Array
+
+
 # size of a tile
 var tileSize : float = 64.0
 
@@ -57,6 +61,7 @@ func get_tile_at_position2(position):
 
 # highlights the tiles we can place buildings on
 func highlight_available_tiles(building_to_place: int):
+	tilesToHighlight.clear()
 	# loop through all of the tiles with buildings
 	for x in range(tilesWithBuildings.size()):
 		#can't place same building next to each other
@@ -64,10 +69,17 @@ func highlight_available_tiles(building_to_place: int):
 			continue
 		# get the tile north, south, east and west of this one
 		for adjacent in adjacents:
-			var tile = get_tile_at_position(tilesWithBuildings[x].position+ adjacent)	
+			var tile = get_tile_at_position(tilesWithBuildings[x].position + adjacent)	
 			# if not null, toggle their highlight - allowing us to build
 			if tile != null:
-				tile.toggle_highlight(true)
+				tilesToHighlight.append(tile)
+				#tile.toggle_highlight(true)
+	for x in range(tilesToHighlight.size()-1, 0, -1):
+		# get the tile north, south, east and west of this one
+		for adjacent in adjacents:
+			var tile = get_tile_at_position(tilesToHighlight[x].position + adjacent)
+			if tile!= null and tile.get_building_type() == building_to_place:
+				tilesToHighlight.remove_at(x)
 
 
 # disables all of the tile highlights
@@ -84,17 +96,18 @@ func place_building(tile, texture, buildingType):
 
 
 func place_trees_and_hills():
-	# place random trees
+	# Three different looking trees to be picked at random
 	var tree1 = preload("res://Sprites/Tree1.png")
 	var tree2 = preload("res://Sprites/Tree2.png")
 	var tree3 = preload("res://Sprites/Tree3.png")
-	for x in range(1, 20):
-		place_building(allTiles.pick_random(), [tree1, tree2, tree3].pick_random(), -1)	
 
-	# place random hills
+	# Three different looking hills to be picked at random
 	var hill1 = preload("res://Sprites/Hill1.png")
 	var hill2 = preload("res://Sprites/Hill2.png")
 	var hill3 = preload("res://Sprites/Hill3.png")
+
+	# Place random hills and trees
 	for x in range(1, 20):
+		place_building(allTiles.pick_random(), [tree1, tree2, tree3].pick_random(), -1)	
 		place_building(allTiles.pick_random(), [hill1, hill2, hill3].pick_random(), -2)	
 	
